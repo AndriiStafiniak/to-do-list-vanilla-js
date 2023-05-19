65f8717df5944aa0cdd38351a1d74430dc99876f
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-05-19 14:05:17 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -9,17 +9,41 @@
          done: true,
       },
    ];
+
+
+   const addNewTask = (newTaskContent) => {
+      tasks.push({
+         content: newTaskContent,
+      });
+      render();
+   };
+
+
+   const removeTask = (index) => {
+      tasks.splice(index, 1);
+      render();
+   };
+
    const render = () => {
       let htmlString = "";
       for (const task of tasks) {
          htmlString += `
-         <li ${task.done ? "style = \"text-decoration: line-through\"" : ""}>
+         <li ${task.done ? "style = \"text-decoration: line-through\"" : ""}> <button class="js-remove">Usuń</button>
          ${task.content}
          </li>
          `;
       }
       document.querySelector(".js-tasks").innerHTML = htmlString;
+      const removeButton = document.querySelectorAll(".js-remove")
+
+      removeButton.forEach((removeButton, index) => {
+         removeButton.addEventListener("click", () => {
+            removeTask(index);
+         });
+      });
    };
+
+
    const onForSubmit = (event) => {
       event.preventDefault();
       const newTaskContent = document.querySelector(".js-newTask").value.trim();
@@ -28,12 +52,10 @@
       }
       addNewTask(newTaskContent);
    }
-   const addNewTask = (newTaskContent) => {
-      tasks.push({
-         content: newTaskContent,
-      });
-      render();
-   };
+
+
+
+
    const init = () => {
       render();
       const form = document.querySelector(".js-form");
