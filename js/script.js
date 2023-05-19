fc87bbd2c8a22c44747bf32f0fef969a687ccd1d
Andrii Stafiniak <andrii.stafiniak@gmail.com>
2023-05-19 13:37:34 +0200

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -10,18 +10,35 @@
       },
    ];
    const render = () => {
-      let htmlSring = "";
+      let htmlString = "";
       for (const task of tasks) {
-         htmlSring += `
-         <li>
+         htmlString += `
+         <li ${task.done ? "style = \"text-decoration: line-through\"" : ""}>
          ${task.content}
          </li>
          `;
       }
-      document.querySelector(".js-tasks").innerHTML = htmlSring;
+      document.querySelector(".js-tasks").innerHTML = htmlString;
+   };
+   const onForSubmit = (event) => {
+      event.preventDefault();
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+      if (newTaskContent === "") {
+         return;
+      }
+      addNewTask(newTaskContent);
+   }
+   const addNewTask = (newTaskContent) => {
+      tasks.push({
+         content: newTaskContent,
+      });
+      render();
    };
    const init = () => {
       render();
+      const form = document.querySelector(".js-form");
+      form.addEventListener("submit", onForSubmit)
+
    };
    init();
 }
